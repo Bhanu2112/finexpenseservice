1a6e5f411bc234e3f2b122f499d0d5a77c3235ed
Bhanu2112 <106594899+Bhanu2112@users.noreply.github.com>
2024-09-20 13:53:59 +0530

    Update ExpenseServiceApplication.java

--- src/main/java/com/org/bhanu/expenseservice/ExpenseServiceApplication.java
+++ src/main/java/com/org/bhanu/expenseservice/ExpenseServiceApplication.java
@@ -3,11 +3,11 @@ package com.org.bhanu.expenseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+
 @EnableScheduling
 public class ExpenseServiceApplication {
 
